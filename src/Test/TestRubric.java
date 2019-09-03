44c5e118197a0167f8e2a7024e23876d5db74e84
A. N. TANVIR <35560149+antanvir@users.noreply.github.com>
2019-09-03 14:29:03 +0600

    Add files via upload

--- src/Test/TestRubric.java
+++ src/Test/TestRubric.java
@@ -7,6 +7,9 @@ import gradebook.Rubric;
 class TestRubric {
 	
 	Rubric r = new Rubric();
+	
+	
+	/************** getMidtermWorth() **************/
 
 	@Test
 	void test_getMidtermWorth1() {
@@ -37,12 +40,32 @@ class TestRubric {
 	
 	@Test
 	void test_getMidtermWorth4() {
-		r.setMidtermWorth(55);
+		r.setMidtermWorth(0);
 		double actual = r.getMidtermWorth();
-		double expected = 0.55;
+		double expected = 0.00;
 		assertEquals(expected, actual);
 		
 	}
+	
+	@Test
+	void test_getMidtermWorth5() {
+		r.setMidtermWorth(-1);
+		double actual = r.getMidtermWorth();
+		double expected = -0.01;
+		assertEquals(expected, actual);
+		
+	}
+	
+	@Test
+	void test_getMidtermWorth6() {
+		r.setMidtermWorth(99);
+		double actual = r.getMidtermWorth();
+		double expected = 0.99;
+		assertEquals(expected, actual);
+		
+	}
+	
+	/************** getFinalWorth() **************/
 
 
 }
